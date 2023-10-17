b320a12965174525b867e471822ea52f8bd1a0dc
MudayJ <02jensen.muday@gulllakecs.org>
2023-10-17 08:30:45 -0400

    Add description and welcome print lines

---
+++ ProjectOrangutan.py
@@ -0,0 +1,8 @@
+"""
+Our welcome screen will start our program letting
+drivers know that the InfoTech Center 2023 is loading
+"""
+
+print("\n\nwelcome to our InfoTech centre 2023")
+
+print("\nInfoTech Center 2023 is Loading...")
